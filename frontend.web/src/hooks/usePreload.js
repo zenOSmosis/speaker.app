497208f45958510256229a8ae5fb1f52993853c6
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-07-25 12:18:34 -0600

    Fix issue where usePreload may be called multiple times in Firefox

--- frontend.web/src/hooks/usePreload.js
+++ frontend.web/src/hooks/usePreload.js
@@ -1,23 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Preload from "preload-it";
 
 /**
- * @param {string[]} resources?
+ * @param {string[]} resources? A list of URLs to preload. Note that these are
+ * cached and the list cannot be changed without re-instantiating the hook.
  * @return {Object}
  */
 export default function usePreload(resources = []) {
   const [isPreloaded, _setIsPreloaded] = useState(false);
   const [progress, _setProgress] = useState(0);
 
+  // Cache the resources; This fixes an issue where the image may preload
+  // multiple times in Firefox (version 90 was problematic)
+  const refResources = useRef(resources);
+
   useEffect(() => {
+    const resources = refResources;
+
     if (resources.length) {
       const preload = new Preload();
 
-      preload.onprogress = (evt) => {
+      preload.onprogress = evt => {
         _setProgress(evt.progress);
       };
 
-      preload.oncomplete = (items) => {
+      preload.oncomplete = items => {
         _setIsPreloaded(true);
       };
 
@@ -26,7 +33,7 @@ export default function usePreload(resources = []) {
       _setIsPreloaded(true);
       _setProgress(100);
     }
-  }, [resources]);
+  }, []);
 
   return {
     isPreloaded,
