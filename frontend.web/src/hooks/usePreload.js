e1e2c68bb19bad1f4fa3b996fd0715b71c85ec21
jzombie <jeremy.harris@zenosmosis.com>
2021-07-25 14:37:39 -0400

    Fix issue where intro logo image would continuously re-fetch with Firefox (#39)
    * Fix issue where usePreload may be called multiple times in Firefox
    
    * Update comments
    
    * Make passed resources non-optional
    
    * Remove hyphen in console message

--- frontend.web/src/hooks/usePreload.js
+++ frontend.web/src/hooks/usePreload.js
@@ -1,23 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Preload from "preload-it";
 
 /**
- * @param {string[]} resources?
+ * Pre-loads resources, as a React hook.
+ *
+ * @param {string[]} resources An array of URLs to preload. Note that these are
+ * cached and the list cannot be changed without re-instantiating the hook.
  * @return {Object}
  */
-export default function usePreload(resources = []) {
+export default function usePreload(resources) {
   const [isPreloaded, _setIsPreloaded] = useState(false);
   const [progress, _setProgress] = useState(0);
 
+  // Cache the resources; This fixes an issue where passing in a non-memoized
+  // array could cause the following useEffect to trigger more than once.
+  //
+  // Issue was discovered when images would preload multiple times in Firefox.
+  const refResources = useRef(resources);
+
   useEffect(() => {
+    const resources = refResources;
+
     if (resources.length) {
       const preload = new Preload();
 
-      preload.onprogress = (evt) => {
+      preload.onprogress = evt => {
         _setProgress(evt.progress);
       };
 
-      preload.oncomplete = (items) => {
+      preload.oncomplete = items => {
         _setIsPreloaded(true);
       };
 
@@ -26,7 +37,7 @@ export default function usePreload(resources = []) {
       _setIsPreloaded(true);
       _setProgress(100);
     }
-  }, [resources]);
+  }, []);
 
   return {
     isPreloaded,
