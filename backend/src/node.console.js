69e934dd8acd3562b23f9d8e8f230aa8bb71ce3f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-06 12:18:47 -0600

    Extracted node.console into separate file

---
+++ backend/src/node.console.js
@@ -0,0 +1,39 @@
+import path from "path";
+
+/**
+ * Node.js console debugger.
+ *
+ * Provides support for additional log method with line number support.
+ *
+ * NOTE: This may be replaced w/ something else.  It is here to make it easier
+ * to debug.
+ *
+ * @see https://stackoverflow.com/questions/45395369/how-to-get-console-log-line-numbers-shown-in-nodejs/60305881#60305881
+ */
+["debug", "log", "warn", "error"].forEach(methodName => {
+  // NOTE: This is not portable across projects; This number is not typical for
+  // Node.js' process.env, and is derived from Speaker.app's backend
+  // index.post-esm.js script
+  const { CPU_NO } = process.env;
+
+  const originalLoggingMethod = console[methodName];
+  console[methodName] = (firstArgument, ...otherArguments) => {
+    const originalPrepareStackTrace = Error.prepareStackTrace;
+    Error.prepareStackTrace = (_, stack) => stack;
+    const callee = new Error().stack[1];
+    Error.prepareStackTrace = originalPrepareStackTrace;
+    const relativeFileName = path.relative(process.cwd(), callee.getFileName());
+
+    const prefix = `${relativeFileName}:${callee.getLineNumber()}:${
+      CPU_NO !== undefined ? `CPU(${CPU_NO}):` : ``
+    }`;
+
+    if (typeof firstArgument === "string") {
+      originalLoggingMethod(prefix + " " + firstArgument, ...otherArguments);
+    } else {
+      originalLoggingMethod(prefix, firstArgument, ...otherArguments);
+    }
+  };
+});
+
+console.debug("Initialized console override debugger");
