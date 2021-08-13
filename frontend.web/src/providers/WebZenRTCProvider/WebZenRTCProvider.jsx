ce3e619c999aab5718ff4ac59858889678aa589e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-12 18:15:52 -0600

    Move WebRTCProvider into child directory

--- frontend.web/src/providers/WebZenRTCProvider/WebZenRTCProvider.jsx
+++ frontend.web/src/providers/WebZenRTCProvider/WebZenRTCProvider.jsx
@@ -15,7 +15,7 @@ import WebZenRTCPeer, {
   EVT_OUTGOING_MEDIA_STREAM_TRACK_REMOVED,
   EVT_INCOMING_MEDIA_STREAM_TRACK_ADDED,
   EVT_INCOMING_MEDIA_STREAM_TRACK_REMOVED,
-} from "../WebZenRTCPeer";
+} from "../../WebZenRTCPeer";
 
 // TODO: Implement Screen Wake Lock API
 // @see https://developer.mozilla.org/en-US/docs/Web/API/Screen_Wake_Lock_API
@@ -282,6 +282,16 @@ export default function WebZenRTCProvider({
         outgoingAudioMediaStreamTracks,
         writableSyncObject,
         readOnlySyncObject,
+
+        // TODO: Implement
+        /*
+        addSelectedMediaStreamTrackController,
+        removeSelectedMediaStreamTrackController,
+
+        addTestingMediaStreamTrackController,
+        removeTestingMediaStreamTrackController,
+        */
+
         // TODO: Don't pass latency this way (it changes often, and entire app shouldn't re-render because of it)
         latency,
       }}
