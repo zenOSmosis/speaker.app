e0e11c6085f1f76e1b0e705c173292059042a4f3
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-14 08:10:04 -0600

    Add TODO

--- frontend.web/src/utils/getIsDevelopmentMode.js
+++ frontend.web/src/utils/getIsDevelopmentMode.js
@@ -1,4 +1,7 @@
 /**
+ * TODO: Move this to PhantomCore as a static method (or equiv) so that the
+ * logger can use this.
+ *
  * Determines if the current React environment is a development environment.
  *
  * @return {boolean}
