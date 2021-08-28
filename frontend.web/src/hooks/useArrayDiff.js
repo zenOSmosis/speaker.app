9565b66be8361271c23757665ecbd20b228e3f47
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-28 08:34:45 -0600

    Document useArrayDiff

--- frontend.web/src/hooks/useArrayDiff.js
+++ frontend.web/src/hooks/useArrayDiff.js
@@ -2,12 +2,25 @@ import { PhantomCollection } from "phantom-core";
 import { useMemo } from "react";
 import usePrevious from "./usePrevious";
 
-// TODO: Document
+/**
+ * @typedef {Object} ArrayDiff
+ * @property {any[]} added Elements added since last change
+ * @property {any[]} removed Elements removed since last change
+ *
+ * @param {any[]} next
+ * @return {ArrayDiff}
+ */
 export default function useArrayDiff(next) {
   // NOTE: Using empty array for previous default value type
   const prev = usePrevious(next, []);
 
   const { added, removed } = useMemo(() => {
+    // NOTE: This check is inside the useMemo so it is not re-run more often
+    // than it should
+    if (!Array.isArray(next)) {
+      throw new Error("next should be an array");
+    }
+
     const { added, removed } = PhantomCollection.getChildrenDiff(prev, next);
 
     return { added, removed };
