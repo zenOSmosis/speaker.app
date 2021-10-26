dac52a57fe78692148c2628fae6a53a0331e64b7
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-26 09:21:48 -0500

    Add TODO

--- backend/src/socketAPI/device/index.js
+++ backend/src/socketAPI/device/index.js
@@ -2,6 +2,8 @@ import DeviceDetector from "device-detector-js";
 
 const deviceDetector = new DeviceDetector();
 
+// TODO: Look into alternative UAParser.js: https://www.npmjs.com/package/ua-parser-js instead
+
 /**
  * @see https://www.npmjs.com/package/device-detector-js
  */
